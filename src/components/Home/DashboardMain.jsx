502d66b54edbad81dc61a6d303a35e0340ac804a
francis-xavier-inyangat <xavierfranzings@gmail.com>
2023-07-17 21:02:19 +0300

    all

--- src/components/Home/DashboardMain.jsx
+++ src/components/Home/DashboardMain.jsx
@@ -6,7 +6,7 @@ const DashboardMain = () => {
     <Box sx={{ width: "100%", height: "20%", bgcolor:'#014d4e',color:'#fff', p: "20px" }}>
       <Typography fontSize="16px">Dashboard</Typography>
       <Typography fontSize="12px">
-        Welcome VITC_ADMIN into TOTAL CARE EUROPE UNIPESSOAL LDA Admin dashboard
+        Welcome VITC_ADMIN into TOTAL CARE EUROPE UNIPESSOAL LDA Admin dashboard..
       </Typography>
     </Box>
   );
